4fd8b798472371839a17da3cdd06d839eca4ffa0
Ruslan0111 <gadilovruslan@yandex.ru>
2021-12-08 22:02:47 +0500

    Testing vue dashboard

--- js/main.js
+++ js/main.js
@@ -1,38 +1,38 @@
-let menuBurger = document.querySelector('.header__burger');
-let header = document.querySelector('.header');
+let menuBurger = document.querySelector(".header__burger");
+let header = document.querySelector(".header");
 
-menuBurger.addEventListener('click', function() {
-  this.classList.toggle('active');
-  document.querySelector('.header__menu').classList.toggle('active');
-  document.querySelector('body').classList.toggle('lock');
+menuBurger.addEventListener("click", function () {
+  this.classList.toggle("active");
+  document.querySelector(".header__menu").classList.toggle("active");
+  document.querySelector("body").classList.toggle("lock");
   header.scrollIntoView({
-    block: 'start',
-    behavior: "smooth"
+    block: "start",
+    behavior: "smooth",
   });
 });
 
-let scrollDownBtn = document.querySelector('.main__scroll-down');
+let scrollDownBtn = document.querySelector(".main__scroll-down");
 
-scrollDownBtn.addEventListener('click', function() {
+scrollDownBtn.addEventListener("click", function () {
   header.scrollIntoView({
-    block: 'start',
-    behavior: "smooth"
+    block: "start",
+    behavior: "smooth",
   });
 });
 
-let menuList = document.querySelector('.header__menu');
+let menuList = document.querySelector(".header__menu");
 
-menuList.addEventListener('click', function(e) {
-  if (!e.target.closest('li')) return;
+menuList.addEventListener("click", function (e) {
+  if (!e.target.closest("li")) return;
 
   e.preventDefault();
-  let a = e.target.closest('li');
+  let a = e.target.closest("li");
   let section = document.getElementById(a.dataset.href);
   section.scrollIntoView({
-    block: 'start',
-    behavior: "smooth"
+    block: "start",
+    behavior: "smooth",
   });
-  menuBurger.classList.toggle('active');
-  document.querySelector('.header__menu').classList.toggle('active');
-  document.querySelector('body').classList.toggle('lock');
+  menuBurger.classList.toggle("active");
+  document.querySelector(".header__menu").classList.toggle("active");
+  document.querySelector("body").classList.toggle("lock");
 });
